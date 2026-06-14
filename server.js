3400d28641cdd5f742e3da2756131d76493331ab
Nguyen Gia Bao <24100074@st.phenikaa-uni.edu.vn>
2026-06-14 10:30:41 +0000

    add SQL

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const db = require("./localdb");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server OK");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
