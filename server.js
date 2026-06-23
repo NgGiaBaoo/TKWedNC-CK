812a7f6081f554ea19c5109a63389876e830b3b4
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2026-06-23 06:33:48 +0000

    Update Cookie and signedCookie

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const db = require("./localdb");
 const registerJobModule = require("./src/jobs/jobModule");
 const registerApplicationModule = require("./src/applications/applicationModule");
@@ -7,6 +8,7 @@ const registerCandidateModule = require("./src/candidates/candidateModule");
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser('your-secret-key'));
 
 app.get("/", (req, res) => {
     res.send("Server OK");
